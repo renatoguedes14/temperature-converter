dd549ec6400315e9f05990cdbefbc636ea5a6e3e
Renato Guedes <renato.guedes14@gmail.com>
2022-12-15 20:33:14 -0300

    feat(project): initial structure

---
+++ src/main/java/br/renato/converter/TemperatureConverterApplication.java
@@ -0,0 +1,13 @@
+package br.renato.converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/renato/converter/TemperatureConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.renato.converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
